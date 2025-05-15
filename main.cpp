e06c1773bfa275cf5d1551146ce3b11766a7cc92
Ruanrpm <ruan.pablo.martinz@gmail.com>
2025-05-15 20:52:22 -0300

    Adicionado Solicitações de voos.

--- main.cpp
+++ main.cpp
@@ -5,35 +5,55 @@
 
 using namespace std;
 
-void PreencheSolicitacoes(no **SP, no **SD) {
+// função para preencher as filas de solicitação de pouso;
+void PreencheSolicitacoes(no **SPeD) {
     srand(time(0));
-
     
     for (int i = 0; i < 27; i++) {
-        int aleat = 1 + (rand() % 2);
         int aleatPrior = 1 + (rand() % 2);
-        if (aleat == 1) {
-            *SP = insereFilaChegada(*SP , i+10 , aleatPrior);
-        }
-        else {
-            *SD = insereFilaChegada(*SD , i+10 , aleatPrior);
-        }
+        *SPeD = insereFilaChegada(*SPeD , i+10 , aleatPrior);
     }
 }
 
 int main() {
-    no *SolicitacoesPouso;
-    no *SolicitacoesDecolagem;
+    bool continua = true;
+    no *FP1, *FP2;
+    no *SolicitacoesPeD;
+    int num, prioridade;
 
-    SolicitacoesPouso = inicializaFP(SolicitacoesPouso);
-    SolicitacoesDecolagem = inicializaFP(SolicitacoesDecolagem);
+    FP1 = inicializaFP(FP1);
+    FP2 = inicializaFP(FP2);
+    SolicitacoesPeD = inicializaFP(SolicitacoesPeD);
 
-    PreencheSolicitacoes(&SolicitacoesPouso , &SolicitacoesDecolagem);
+    PreencheSolicitacoes(&SolicitacoesPeD);
 
-    cout << "solicitcoes de POUSO:" <<endl;
-    exibe(SolicitacoesPouso);
+    cout << "Solicitacoes: ";
+    exibe(SolicitacoesPeD);
     cout <<endl;
-    cout << "--------------------------------" <<endl;
-    cout << "solicitcoes de DECOLAGEM:" <<endl;
-    exibe(SolicitacoesDecolagem);
+
+    for (int ut = 1; continua; ut++) {
+        srand(time(0));
+        // Quantidade de solicitações de pouso por ut;
+        for (int solicitacoes = 0; solicitacoes < 5; solicitacoes++) {
+            // uso do rand() para decidir se é uma solicitação de pouso ou decolagem;
+            int aleat = 1 + (rand() % 2);
+            if (aleat == 1) {
+                if (!verificaSeVazia(SolicitacoesPeD)) {
+                    SolicitacoesPeD = removeFP(SolicitacoesPeD, &num, &prioridade);
+                    FP1 = insereFP(FP1, num, prioridade);
+                }  
+            }
+            else {
+                if (!verificaSeVazia(SolicitacoesPeD)) {
+                    SolicitacoesPeD = removeFP(SolicitacoesPeD, &num, &prioridade);
+                    FP2 = insereFP(FP2, num, prioridade);
+                }
+            }
+        }
+    }
+    cout <<endl << "Fila de solicitacoes de POUSO:";
+    exibe(FP1);
+    cout <<endl;
+    cout << "Fila de solicitacoes de Decolagem:";
+    exibe(FP2);
 }
